a08e09b1b0fed458e5362c1dda496d8503fa4fb0
NagamuniVemi <160997589+NagamuniVemi@users.noreply.github.com>
2024-02-23 06:25:20 +0000

    addind datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,11 @@
+//datatypes
+"use strict";//new standards treats by the compiler
+/* symbol       --makes unique objects for the variables
+   string        -stores values
+   number        --stores int values
+   boolean        --true or false
+   bigint        --large values
+   null          --satnd alone value and also atype
+   undefined      --value is not assigned
+*/
+console.log(typeof null)
--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,16 @@
 console.log("muni")
+//variables
+const name1="nagamuni"
+var email="nagamunireddyofficial@gmail.com"
+let phoneNo=9491167823
+let muni1;
+//constant values doesnot changable once declared
+
+
+console.table([name1,email,phoneNo,muni1])
+
+//var is not used becauseit scope access in any where it changes all values 
+
+
+
+//data types
