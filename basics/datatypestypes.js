c46954724e5e172dc244792f09136bcd5a2d8d0b
NagamuniVemi <160997589+NagamuniVemi@users.noreply.github.com>
2024-02-23 07:28:46 +0000

    types of datatypes

---
+++ basics/datatypestypes.js
@@ -0,0 +1,27 @@
+/* 
+javascript is a dynamic typed languge
+
+
+primitive 
+
+
+    7-types->string,boolean,bigint,number,null,undefined,symbol
+
+
+REFRENCE TYPE/NON PRIMITIVE
+
+
+    Arrays ,objects,functions
+    
+
+*/
+const heros=["mui"]
+const her0={
+    nname1:"hitesh",
+
+}
+const m=function()
+{
+    console.log("muni")
+}
+console.log(typeof m)
