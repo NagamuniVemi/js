e6d208f5011d4c2ffbce32d8172fb09578d3d5b7
NagamuniVemi <160997589+NagamuniVemi@users.noreply.github.com>
2024-02-23 07:14:21 +0000

    comparision

---
+++ basics/comparisons.js
@@ -0,0 +1,10 @@
+//comparision operators
+//string datatypes compares with numbers 
+console.log(null==0)
+console.log(null>=0)  //null value is >=0
+console.log(null<0)
+
+//strict check ===    itchecks the datatype not the data 
+
+
+
