a28857687fb01e8ae4b457bcbc4cf2871e46d134
NagamuniVemi <160997589+NagamuniVemi@users.noreply.github.com>
2024-02-23 07:38:30 +0000

    memory

---
+++ basics/stacksandheaps.js
@@ -0,0 +1,4 @@
+//memory are two types 
+//stack(primitive)->copy   ,  heap(nonprimitive )->adress
+// in primiive copy value you pass so changes not appeared in original
+// in nonprimitive changes appear in the original also because all point to same value so that you change one value it wiilreflect on th other
